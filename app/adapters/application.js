cfdd8cf18837373f30510e398ddbcc60d1249211
David Binetti <dbinetti@gmail.com>
2016-01-08 18:57:13 -0800

    API Proxy

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,4 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-   // host: window.BarberscoreEmber.API_HOST,
-   // namespace: window.BarberscoreEmber.API_NAMESPACE,
-   host: 'http://barberscore.herokuapp.com',
-   namespace: 'api',
 });
