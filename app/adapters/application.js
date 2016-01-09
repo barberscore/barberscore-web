3ff7414d02ce72cf3c766c900433ae8b042ae1ba
David Binetti <dbinetti@gmail.com>
2016-01-09 02:27:50 -0800

    Test config in prod

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,6 +2,9 @@ import DS from 'ember-data';
 import ENV from '../config/environment';
 
 console.log(ENV.APP.API_HOST);
+console.log(ENV.APP.API_NAMESPACE);
 
 export default DS.JSONAPIAdapter.extend({
+  host: ENV.APP.API_HOST,
+  namespace: ENV.APP.API_NAMESPACE,
 });
