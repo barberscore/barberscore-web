8b9cdcf15cfc44caf75a37839b9c3cc0b14644ef
David Binetti <dbinetti@gmail.com>
2016-01-21 20:25:44 -0800

    Navbar collapse

---
+++ app/components/navbar-block.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  isCollapsed: true,
+  actions: {
+    toggleCollapsed() {
+      this.toggleProperty('isCollapsed');
+    }
+  }
+});
