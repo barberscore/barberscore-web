0ac114b33e3f45d551bed822b529e80d00fec513
David Binetti <dbinetti@gmail.com>
2016-01-29 15:16:42 -0800

    Remove 'rounds' from Contest model

--- app/models/contest.js
+++ app/models/contest.js
@@ -6,7 +6,6 @@ export default DS.Model.extend({
   goal: DS.attr('contest-goal'),
   qual_score: DS.attr('number'),
   year: DS.attr('number'),
-  rounds: DS.attr('number'),
   session: DS.belongsTo('session', {async: true}),
   award: DS.belongsTo('award', {async: true}),
   contestants: DS.hasMany('contestant', {async: true}),
