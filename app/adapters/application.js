b95d59c8fc1c6998aeb5f1d1a02ebcb2fa7e7a8f
David Binetti <dbinetti@gmail.com>
2016-02-02 09:55:51 -0800

    Revert "WIP [ci skip]"
    This reverts commit 5451f61bc7db41cf2c952122a49ad6b6e496fffa.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,10 +1,8 @@
 import DS from 'ember-data';
 import ENV from '../config/environment';
-import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+export default DS.JSONAPIAdapter.extend({
   host: ENV.APP.API_HOST,
   namespace: ENV.APP.API_NAMESPACE,
   coalesceFindRequests: false,
-  authorizer: 'authorizer:oauth2'
 });
--- app/router.js
+++ app/router.js
@@ -6,7 +6,6 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('login');
   this.route('about');
   this.route('faq');
 
