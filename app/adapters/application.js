5451f61bc7db41cf2c952122a49ad6b6e496fffa
David Binetti <dbinetti@gmail.com>
2016-02-02 07:21:04 -0800

    WIP [ci skip]

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,10 @@
 import DS from 'ember-data';
 import ENV from '../config/environment';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.JSONAPIAdapter.extend({
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   host: ENV.APP.API_HOST,
   namespace: ENV.APP.API_NAMESPACE,
   coalesceFindRequests: false,
+  authorizer: 'authorizer:oauth2'
 });
