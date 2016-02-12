6ac8f4734f6026dd6dd06c14c653b0a00f202eb9
David Binetti <dbinetti@gmail.com>
2016-02-12 15:46:48 -0800

    Through awards

---
+++ app/components/performer-status.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  store: Ember.inject.service(),
+  actions: {
+    saveRecord(status) {
+      this.model.set('status', status);
+      const flashMessages = Ember.get(this, 'flashMessages');
+      this.model.save()
+      .then(() => {
+        flashMessages.success('Success');
+      })
+      .catch(() => {
+        flashMessages.danger('Error');
+      });
+    },
+  },
+  performerStatus: [
+    'New',
+    'Qualified',
+    'Accepted',
+    'Declined',
+    'Dropped',
+    'Official',
+    'Finished',
+    'Final',
+  ]
+});
