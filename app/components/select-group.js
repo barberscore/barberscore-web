88beb74015507009bd477434319f604fb69906f1
David Binetti <dbinetti@gmail.com>
2016-02-12 13:28:19 -0800

    Add groups to performers

---
+++ app/components/select-group.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  store: Ember.inject.service(),
+  actions: {
+    saveRecord(group) {
+      this.model.set('group', group);
+      const flashMessages = Ember.get(this, 'flashMessages');
+      this.model.save()
+      .then(() => {
+        flashMessages.success('Success');
+      })
+      .catch(() => {
+        flashMessages.danger('Error');
+      });
+    },
+    searchGroup(term) {
+      return new Ember.RSVP.Promise((resolve, reject) => {
+        Ember.run.debounce(this, this._performSearch, term, resolve, reject, 600);
+      });
+    }
+  },
+  _performSearch(term, resolve, reject) {
+    if (Ember.isBlank(term)) { return resolve([]); }
+    this.get('store').query('group', {'name__icontains': term})
+      .then(data => resolve(data), reject);
+  },
+});
