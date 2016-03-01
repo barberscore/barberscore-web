f3ce738913a25bf6ae2372bbab7a05c58fef395e
David Binetti <dbinetti@gmail.com>
2016-03-01 14:01:36 -0800

    Find 2016 on public site

--- app/routes/conventions.js
+++ app/routes/conventions.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.store.findAll('convention');
+    return this.store.query('convention', {year:2016});
   }
 });
