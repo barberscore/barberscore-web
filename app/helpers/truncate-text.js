725f126911ad2b13dcb033bae0cfb99b3e6ddd53
David Binetti <dbinetti@gmail.com>
2016-03-11 11:45:27 -0800

    Add truncate

---
+++ app/helpers/truncate-text.js
@@ -0,0 +1,20 @@
+export function truncateText(params, hash) {
+  var value = params[0];
+  var len = hash.limit;
+  var out = '';
+
+  if (value !== undefined) {
+    out = value.substr(0, len);
+
+    if (value.length > len) {
+      out += '...';
+    }
+
+  } else {
+    out = '';
+  }
+
+  return out;
+}
+
+export default Ember.Helper.helper(truncateText);
