90566a2f88f9a09d00674cf3b737fa36cdaef093
David Binetti <dbinetti@gmail.com>
2016-03-14 09:55:25 -0700

    Update year display

--- app/routes/conventions/index.js
+++ app/routes/conventions/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.store.query('convention', {year__gte: 2015, year__lte: 2016});
+    return this.store.query('convention', {year__lte: 2016});
   },
 });
