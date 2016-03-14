d11b2664cef022175d973aea9ed7afc3c11287b9
David Binetti <dbinetti@gmail.com>
2016-03-14 11:49:29 -0700

    Add convention restriction to Admin

--- app/routes/admin/index.js
+++ app/routes/admin/index.js
@@ -3,6 +3,6 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model: function() {
-    return this.store.query('convention', {year__gte: 2015, year__lte: 2016});
+    return this.store.query('convention', {year: 2016, season: 4});
   },
 });
