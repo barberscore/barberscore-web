f44193fabcf589da5620478db834783819a7a525
David Binetti <dbinetti@gmail.com>
2016-03-14 22:54:44 -0700

    CA and ACA search

--- app/components/contest-admin.js
+++ app/components/contest-admin.js
@@ -22,7 +22,7 @@ export default Ember.Component.extend({
   },
   _performSearch(term, resolve, reject) {
     if (Ember.isBlank(term)) { return resolve([]); }
-    this.get('store').query('person', {'name__icontains': term})
+    this.get('store').query('person', {'name__icontains': term, 'certifications__category': 1})
       .then(data => resolve(data), reject);
   },
 });
