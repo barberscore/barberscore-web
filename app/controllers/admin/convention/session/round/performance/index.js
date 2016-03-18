88e6a8a8a86bfb4bf47c2a3fb680926c24889f32
David Binetti <dbinetti@gmail.com>
2016-03-18 10:44:51 -0700

    disable controller

--- app/controllers/admin/convention/session/round/performance/index.js
+++ app/controllers/admin/convention/session/round/performance/index.js
@@ -1,36 +1,36 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  store: Ember.inject.service(),
-  songOne: Ember.computed.filterBy('model.songs', 'order', 'First'),
-  songTwo: Ember.computed.filterBy('model.songs', 'order', 'Second'),
-  allSongs: this.model.get('performer.submissions.@each.chart'),
-  actions: {
-    saveOne(subOne) {
-      // console.log(subOne.get('chart.name'));
-      // console.log(this.model);
-      console.log(this.allSongs);
-      // s1.set('chart', subOne.get('chart'));
-      // const flashMessages = Ember.get(this, 'flashMessages');
-      // s1.save();
-      // .then(() => {
-      //   flashMessages.success('Success');
-      // })
-      // .catch(() => {
-      //   flashMessages.danger('Error');
-      // });
-    },
-    saveTwo(subTwo) {
-      console.log(subTwo.get('chart.name'));
-      // song.set('chart', submission.gsubet('chart'));
-      // const flashMessages = Ember.get(this, 'flashMessages');
-      // song.save()
-      // .then(() => {
-      //   flashMessages.success('Success');
-      // })
-      // .catch(() => {
-      //   flashMessages.danger('Error');
-      // });
-    },
-  },
+  // store: Ember.inject.service(),
+  // songOne: Ember.computed.filterBy('model.songs', 'order', 'First'),
+  // songTwo: Ember.computed.filterBy('model.songs', 'order', 'Second'),
+  // allSongs: this.model.get('performer.submissions.@each.chart'),
+  // actions: {
+  //   saveOne(subOne) {
+  //     // console.log(subOne.get('chart.name'));
+  //     // console.log(this.model);
+  //     console.log(this.allSongs);
+  //     // s1.set('chart', subOne.get('chart'));
+  //     // const flashMessages = Ember.get(this, 'flashMessages');
+  //     // s1.save();
+  //     // .then(() => {
+  //     //   flashMessages.success('Success');
+  //     // })
+  //     // .catch(() => {
+  //     //   flashMessages.danger('Error');
+  //     // });
+  //   },
+  //   saveTwo(subTwo) {
+  //     console.log(subTwo.get('chart.name'));
+  //     // song.set('chart', submission.gsubet('chart'));
+  //     // const flashMessages = Ember.get(this, 'flashMessages');
+  //     // song.save()
+  //     // .then(() => {
+  //     //   flashMessages.success('Success');
+  //     // })
+  //     // .catch(() => {
+  //     //   flashMessages.danger('Error');
+  //     // });
+  //   },
+  // },
 });
