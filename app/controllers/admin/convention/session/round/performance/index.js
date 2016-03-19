4a83a7e2db9b6b3facf685323224570d0458f304
David Binetti <dbinetti@gmail.com>
2016-03-19 08:48:59 -0700

    Sort judges in performance

--- app/controllers/admin/convention/session/round/performance/index.js
+++ app/controllers/admin/convention/session/round/performance/index.js
@@ -1,7 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  // store: Ember.inject.service(),
+  store: Ember.inject.service(),
+  judgeSortProperties: ['slot',],
+  sortedJudges: Ember.computed.sort(
+    'model.round.session.judges',
+    'judgeSortProperties'
+  ),
+
+
+
+
   // songOne: Ember.computed.filterBy('model.songs', 'order', 'First'),
   // songTwo: Ember.computed.filterBy('model.songs', 'order', 'Second'),
   // allSongs: this.model.get('performer.submissions.@each.chart'),
