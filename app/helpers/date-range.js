d87b1e7296f31756e0c994b18ff158310c70a320
David Binetti <dbinetti@gmail.com>
2016-04-08 09:58:18 -0700

    Fix front-end date time reference

--- app/helpers/date-range.js
+++ app/helpers/date-range.js
@@ -3,7 +3,7 @@ import moment from 'moment';
 
 export function dateRange(params) {
   if (params[0]) {
-    return moment(params[0].lower).format('LL') + ' - ' + moment(params[0].upper).subtract(1, 'days').format('LL');
+    return moment(params[0].lower).format('LL') + ' - ' + moment(params[0].upper).format('LL');
   } else {
     return "";
   }
