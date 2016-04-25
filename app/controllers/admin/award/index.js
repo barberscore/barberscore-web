4eaf12a77070b75b41e3f1b596225a1b704255e1
David Binetti <dbinetti@gmail.com>
2016-04-25 13:22:45 -0700

    Entrant interface

--- app/controllers/admin/award/index.js
+++ app/controllers/admin/award/index.js
@@ -2,15 +2,42 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
+    newAward() {
+      let newAward = this.store.createRecord(
+        'award'
+      );
+      this.set('model', newAward);
+      this.set('isEditing', true);
+    },
+    editAward() {
+      this.set('isEditing', true);
+    },
+    cancelAward() {
+      this.model.rollbackAttributes();
+      this.set('isEditing', false);
+    },
+    deleteAward() {
+      const flashMessages = Ember.get(this, 'flashMessages');
+      this.model.destroyRecord()
+      .then(() => {
+        flashMessages.warning('Deleted');
+        this.transitionToRoute('admin');
+      })
+      .catch(() => {
+        flashMessages.danger('Error');
+      });
+    },
     saveAward() {
       const flashMessages = Ember.get(this, 'flashMessages');
       this.model.save()
       .then(() => {
-        flashMessages.success("Saved" );
+        this.transitionToRoute('admin.award', this.model);
+        this.set('isEditing', false);
+        flashMessages.success('Saved');
       })
       .catch(() => {
-        flashMessages.danger("Error" );
+        flashMessages.danger('Error');
       });
-    }
+    },
   },
 });
