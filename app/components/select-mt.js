f891ed6d3bddd8ba36c5d4323930232aed8d04a7
David Binetti <dbinetti@gmail.com>
2016-05-09 14:18:05 -0700

    Add mic tester and submission with-create

---
+++ app/components/select-mt.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  store: Ember.inject.service(),
+  actions: {
+    searchGroup(term) {
+      return new Ember.RSVP.Promise((resolve, reject) => {
+        Ember.run.debounce(this, this._performSearch, term, resolve, reject, 600);
+      });
+    },
+   createGroup(groupName) {
+      let newGroup = this.get('store').createRecord('group', {
+        name: groupName,
+      });
+      newGroup.save();
+      this.set('group', newGroup);
+    },
+  },
+  _performSearch(term, resolve, reject) {
+    if (Ember.isBlank(term)) { return resolve([]); }
+    this.get('store').query('group', {'chap_name__icontains': term})
+      .then(data => resolve(data), reject);
+  },
+});
--- app/components/select-submission.js
+++ app/components/select-submission.js
@@ -1,11 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
   sortSubmissions: [
     'name:asc',
   ],
   submissionChoices: Ember.computed.sort(
     'model.performer.submissions',
     'sortSubmissions'
-  )
+  ),
+  actions: {
+   createSubmission(submissionTitle) {
+      let cht = this.get('store').queryRecord('chart', {
+        name: submissionTitle
+      });
+      let per  = this.get('model.performer');
+      let newSubmission = this.get('store').createRecord('submission', {
+        performer: per,
+        chart: cht
+      });
+      newSubmission.save();
+      this.set('submission', newSubmission);
+    },
+  },
 });
