8f9eae66ff8a256e4b03ef032948ef697b454b83
David Binetti <dbinetti@gmail.com>
2016-07-06 17:59:19 -0500

    Collapse by default

--- app/controllers/admin/convention/session/currents.js
+++ app/controllers/admin/convention/session/currents.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  isHeaderCollapsed: false,
+  isHeaderCollapsed: true,
   actions: {
     collapseHeader() {
       this.toggleProperty('isHeaderCollapsed');
