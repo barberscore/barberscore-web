d1dfd1e565be2b189f8ca4265adf831d27cdd073
David Binetti <dbinetti@gmail.com>
2016-07-19 23:42:19 -0700

    First pass form errors on login

--- app/components/user-login.js
+++ app/components/user-login.js
@@ -2,13 +2,32 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   sess: Ember.inject.service('session'),
-
   actions: {
     authenticate: function() {
+      const flashMessages = Ember.get(this, 'flashMessages');
       var authenticator = 'authenticator:jwt';
       var credentials = this.getProperties('identification', 'password');
 
-      this.get('sess').authenticate(authenticator, credentials);
+      this.get('sess').authenticate(authenticator, credentials).catch((reason) => {
+        let emailErrors = reason.email;
+        let passwordErrors = reason.password;
+        let formErrors = reason.non_field_errors;
+        if (emailErrors) {
+          emailErrors.forEach(function(item){
+            flashMessages.danger("Email: " + item);
+          });
+        }
+        if (passwordErrors) {
+          passwordErrors.forEach(function(item){
+            flashMessages.danger("Password: " + item);
+          });
+        }
+        if (formErrors) {
+          formErrors.forEach(function(item){
+            flashMessages.danger("Error: " + item);
+          });
+        }
+      });
     }
   }
 });
