4f1895f24bce5e055f07a5a60e4b5263f94b8978
David Binetti <dbinetti@gmail.com>
2016-10-21 06:00:33 -0700

    Fix regeneratorRuntime error

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,9 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     'ember-power-select': {
       theme: 'bootstrap'
+    },
+    babel: {
+      includePolyfill: true
     }
   });
 
