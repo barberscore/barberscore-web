0e0147beba22534400422a91e6fcaeddc354055d
David Binetti <dbinetti@gmail.com>
2016-10-23 14:25:37 -0700

    Image upload

---
+++ app/components/file-upload.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+import EmberUploader from 'ember-uploader';
+import config from '../config/environment';
+
+export default EmberUploader.FileField.extend({
+  filesDidChange: function(files) {
+    const host = config.APP.API_HOST;
+    const namespace = config.APP.API_NAMESPACE;
+    const target = this.get('target');
+    const uploader = EmberUploader.Uploader.create({
+      url: `${host}/${namespace}/person/${target}/picture`,
+      // url: 'http://localhost:8000/api/person/04681ad6-f976-4326-8640-61cac4f82e9f/picture',
+      paramName: 'upload'
+    });
+
+    if (!Ember.isEmpty(files)) {
+      // this second argument is optional and can to be sent as extra data with the upload
+      // uploader.upload(files[0]);
+      uploader.upload(files[0]).then(data => {
+        console.log('success');
+      }, error => {
+        console.log('failure');
+      })
+    }
+  }
+});
