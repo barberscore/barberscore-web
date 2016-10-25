fe7d273eaefe50a62c547d7e5047a9bbafd91f22
David Binetti <dbinetti@gmail.com>
2016-10-25 05:02:18 -0700

    Update for Lock 10

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import ApplicationRouteMixin from 'ember-simple-auth-auth0/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
   currentUser: Ember.inject.service('current-user'),
@@ -16,9 +16,43 @@ export default Ember.Route.extend(ApplicationRouteMixin, {
   },
   actions: {
     login () {
-      var lockOptions = {authParams:{scope: 'openid profile'}};
-      this.get('session').authenticate('simple-auth-authenticator:lock', lockOptions);
+      var lockOptions = {
+        autoclose: true,
+        auth: {
+          redirect: false,
+          // I think this may be the key to DRF
+          // redirect: true,
+          // redirectUrl: 'api',
+          // responseMode: 'form_post',
+          // responseType: 'code',
+
+          params: {
+            scope: 'openid email name user_metadata app_metadata picture offline_access',
+            prompt: 'consent'
+          }
+        },
+        additionalSignUpFields: [{
+          name: 'name',
+          placeholder: 'Full Name',
+        }]
+      };
+
+      // var lockOptions = {
+      //   authParams:{
+      //     scope: 'openid email name user_metadata app_metadata picture',
+      //     connections: ['google-oauth2', 'Username-Password-Authentication'],
+      //     connection_scopes: {
+      //       'google-oauth2': [
+      //         'profile',
+      //         'https://www.googleapis.com/auth/drive.photos.readonly',
+      //       ]
+      //     },
+      //     access_type: 'offline',
+      //   },
+      // };
+      this.get('session').authenticate('authenticator:auth0-lock', lockOptions);
     },
+
     logout () {
       this.get('session').invalidate();
     }
