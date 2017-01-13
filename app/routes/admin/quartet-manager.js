fd443b4bfd6c1f8e533281ab04f6b9a5eb4e6d3c
David Binetti <dbinetti@gmail.com>
2017-01-13 10:14:37 -0800

    Update quartet manager

--- app/routes/admin/quartet-manager.js
+++ app/routes/admin/quartet-manager.js
@@ -3,12 +3,6 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model: function() {
-    // let groups = this.store.findAll('group');
-    // return groups.filterBy(
-      // 'kind',
-      // 'Quartet'
-    // );
-    // return this.store.findAll('group').filterBy('kind', 'Quartet');
     return this.get('store').query('group', {kind: 1});
   },
 });
