3dfb6614d1af9374cb2d74ea6ab2f7af84dcc5f5
David Binetti <dbinetti@gmail.com>
2017-01-13 10:10:24 -0800

    Update file-upload component to make resource selectable.

--- app/components/file-upload.js
+++ app/components/file-upload.js
@@ -7,19 +7,17 @@ export default EmberUploader.FileField.extend({
     const host = config.APP.API_HOST;
     const namespace = config.APP.API_NAMESPACE;
     const target = this.get('target');
+    const resource = this.get('resource');
     const uploader = EmberUploader.Uploader.create({
-      url: `${host}/${namespace}/person/${target}/picture`,
-      // url: 'http://localhost:8000/api/person/04681ad6-f976-4326-8640-61cac4f82e9f/picture',
+      url: `${host}/${namespace}/${resource}/${target}/picture`,
       paramName: 'upload'
     });
 
     if (!Ember.isEmpty(files)) {
-      // this second argument is optional and can to be sent as extra data with the upload
-      // uploader.upload(files[0]);
-      uploader.upload(files[0]).then(() => {
-        console.log('success');
-      }).catch(() => {
-        console.log('failure');
+      uploader.upload(files[0])
+      .then(() => {})
+      .catch((error) => {
+        console.log(error);
       });
     }
   }
