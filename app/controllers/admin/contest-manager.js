5e7fc1cb2e50b2ce17e447ebbf4c618d8d8190bd
David Binetti <dbinetti@gmail.com>
2017-01-14 10:00:53 -0800

    Update Contest Manager for PowerSelect UI

--- app/controllers/admin/contest-manager.js
+++ app/controllers/admin/contest-manager.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  store: Ember.inject.service(),
   sessionSortProperties: [
     'convention.start_date:asc',
   ],
@@ -12,5 +13,18 @@ export default Ember.Controller.extend({
     sortBy(sessionSortProperties) {
       this.set('sessionSortProperties', [sessionSortProperties]);
     },
+    transitionSession(session) {
+      this.transitionToRoute('admin.contest-manager.session', session);
+    },
+    searchSession(term) {
+      return new Ember.RSVP.Promise((resolve, reject) => {
+        Ember.run.debounce(this, this._performSearch, term, resolve, reject, 600);
+      });
+    },
+  },
+  _performSearch(term, resolve, reject) {
+    if (Ember.isBlank(term)) { return resolve([]); }
+    this.get('store').query('session', {'nomen__icontains': term})
+      .then(data => resolve(data), reject);
   }
 });
