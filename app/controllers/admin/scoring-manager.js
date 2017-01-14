57efa7e1312f677efd573deca3f992f26e3fc0ef
David Binetti <dbinetti@gmail.com>
2017-01-14 10:00:59 -0800

    Update Scoring Manager for PowerSelect UI

--- app/controllers/admin/scoring-manager.js
+++ app/controllers/admin/scoring-manager.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  store: Ember.inject.service(),
   sessionSortProperties: [
-    'convention.start_date:desc',
+    'convention.start_date:asc',
   ],
   sortedSessions: Ember.computed.sort(
     'model',
@@ -12,5 +13,18 @@ export default Ember.Controller.extend({
     sortBy(sessionSortProperties) {
       this.set('sessionSortProperties', [sessionSortProperties]);
     },
+    transitionSession(session) {
+      this.transitionToRoute('admin.contest-manager.session', session);
+    },
+    searchSession(term) {
+      return new Ember.RSVP.Promise((resolve, reject) => {
+        Ember.run.debounce(this, this._performSearch, term, resolve, reject, 600);
+      });
+    },
+  },
+  _performSearch(term, resolve, reject) {
+    if (Ember.isBlank(term)) { return resolve([]); }
+    this.get('store').query('session', {'nomen__icontains': term})
+      .then(data => resolve(data), reject);
   }
 });
