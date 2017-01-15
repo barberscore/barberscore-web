1bb562c0e1f76582c808e4bc5009e56630a0137d
David Binetti <dbinetti@gmail.com>
2017-01-14 20:34:10 -0800

    PoC Power-Select based discovery UI

--- app/routes/admin/contest-manager.js
+++ app/routes/admin/contest-manager.js
@@ -2,7 +2,4 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  model: function() {
-    return this.store.findAll('session');
-  },
 });
