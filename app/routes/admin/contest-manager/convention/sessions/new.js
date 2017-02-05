f163cd932b5d528b9f010498b912d535eb598d1b
David Binetti <dbinetti@gmail.com>
2017-02-05 14:47:58 -0800

    Pattern for new record creation WIP

--- app/routes/admin/contest-manager/convention/sessions/new.js
+++ app/routes/admin/contest-manager/convention/sessions/new.js
@@ -3,12 +3,20 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   store: Ember.inject.service(),
+  controllerName: 'admin/contest-manager/convention/sessions/session/details',
+  templateName: 'admin/contest-manager/convention/sessions/session/details',
   model: function() {
     let convention_id = this.paramsFor('admin.contest-manager.convention').convention_id;
     let convention = this.get('store').peekRecord('convention', convention_id);
     return this.get('store').createRecord('session', {
       convention: convention,
     });
+  },
+  setupController(controller, model) {
+    // Call _super for default behavior
+    this._super(controller, model);
+    // Implement your custom setup after
+    controller.set('isEditing', true);
   },
   cleanModel: function() {
       var model = this.modelFor( this.routeName );
