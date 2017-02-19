a47fd0f98c701364105e6fac22dab25242953d8b
David Binetti <dbinetti@gmail.com>
2017-02-19 00:11:55 -0800

    Contest-Manger WIP [ci skip]

--- app/controllers/admin/contest-manager.js
+++ app/controllers/admin/contest-manager.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  sortProperties: [
+    'kind:asc',
+  ],
+  sortedSessions: Ember.computed.sort(
+    'model',
+    'sortProperties'
+  ),
+  actions: {
+    sortBy(sortProperties) {
+      this.set('sortProperties', [sortProperties]);
+    },
+  }
 });
