0236f69ec4ee0ff8f68009c23e3fd74f498b21e8
David Binetti <dbinetti@gmail.com>
2017-03-10 08:20:32 -0800

    Testing test coverage

---
+++ tests/unit/models/award-test.js
@@ -0,0 +1,22 @@
+import { moduleForModel, test } from 'ember-qunit';
+
+moduleForModel('award', 'Unit | Model | award', {
+  // Specify the other units that are required for this test.
+  needs: [
+    'model:entity',
+    'model:contest',
+  ]
+});
+
+test('it exists', function(assert) {
+  let model = this.subject();
+
+  assert.ok(!!model);
+});
+
+test('name is foobar', function(assert) {
+  let foobar = this.subject({
+    name: 'Foobar',
+  });
+  assert.equal(foobar.get('name'), 'Foobar', 'Name is Foobar');
+});
