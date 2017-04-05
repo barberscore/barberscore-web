d228ba4495949e70ed4d8232434294a244be79ce
David Binetti <dbinetti@gmail.com>
2017-04-05 11:39:19 -0700

    Catalog creation support

--- app/controllers/admin/catalog-manager/index.js
+++ app/controllers/admin/catalog-manager/index.js
@@ -5,6 +5,7 @@ export default Ember.Controller.extend({
   queryParams: [
     'page_size',
   ],
+  openModal: false,
   page_size: 100,
   sortProperties: [
     'title',
@@ -28,6 +29,44 @@ export default Ember.Controller.extend({
     },
     transitionCatalog(catalog) {
       this.transitionToRoute('admin.catalog-manager.catalog.details', catalog);
+    },
+    createCatalog(){
+      let catalog = this.get('store').createRecord('catalog', {
+        title: this.get('title'),
+        arrangers: this.get('arrangers'),
+        composers: this.get('composers'),
+        holders: this.get('holders'),
+      });
+      catalog.save()
+      .then(() => {
+        this.set('title', null);
+        this.set('arrangers', null);
+        this.set('composers', null);
+        this.set('holders', null);
+        this.set('openModal', false);
+        this.get('flashMessages').success('Success');
+        this.transitionToRoute('admin.catalog-manager.catalog.details', catalog);
+      })
+      .catch(() => {
+        catalog.deleteRecord();
+        this.get('flashMessages').danger('Error');
+      });
+    },
+    clearCatalog() {
+      this.set('title', null);
+      this.set('arrangers', null);
+      this.set('composers', null);
+      this.set('holders', null);
+      this.set('openModal', false);
+    },
+    deleteCatalog(catalog){
+      catalog.destroyRecord()
+      .then(() => {
+        this.get('flashMessages').warning('Deleted');
+      })
+      .catch(() => {
+        this.get('flashMessages').danger('Error');
+      });
     },
   }
 });
