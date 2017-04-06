11f18ae77d0d275f90e285f8e938c7cfc02eadc0
David Binetti <dbinetti@gmail.com>
2017-04-06 12:43:35 -0700

    Sort quartets

--- app/controllers/admin/quartet-manager/index.js
+++ app/controllers/admin/quartet-manager/index.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  sortedQuartetsProperties: [
+    'status:asc',
+    'name'
+  ],
+  sortedQuartets: Ember.computed.sort(
+    'model',
+    'sortedQuartetsProperties'
+  )
 });
