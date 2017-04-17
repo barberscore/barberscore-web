32a640dcb9fef3157354b000bcc5109d4bab7fe3
David Binetti <dbinetti@gmail.com>
2017-04-17 08:25:05 -0700

    Support division award fetching

--- app/routes/dashboard/award-manager/index.js
+++ app/routes/dashboard/award-manager/index.js
@@ -4,10 +4,10 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   currentUser: Ember.inject.service('current-user'),
   model() {
-    let user_id = this.get('currentUser.user.id');
+    let user = this.get('currentUser.user.id');
     return this.get('store').query('award', {
-      'entity__officers__person__user': user_id,
-      'page_size': 100,
+      'drcj': user,
+      'page_size': 1000
     });
   },
 });
