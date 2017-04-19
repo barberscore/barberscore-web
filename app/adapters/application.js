087107977a3b8aad4f91e808764f4d083c18945f
David Binetti <dbinetti@gmail.com>
2017-04-19 06:04:52 -0700

    Bypass requirement of HTTP 422

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -6,5 +6,12 @@ export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   host: ENV.APP.API_HOST,
   namespace: ENV.APP.API_NAMESPACE,
   coalesceFindRequests: true,
-  authorizer: 'authorizer:jwt'
+  authorizer: 'authorizer:jwt',
+  // TODO Probably should have the server return a 422
+  handleResponse: function(status, headers, payload){
+    if(status === 400 && payload.errors){
+      return new DS.InvalidError(payload.errors);
+    }
+    return this._super(...arguments);
+  }
 });
