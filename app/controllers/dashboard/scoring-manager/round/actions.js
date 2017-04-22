9e69e08817941b799655a59556b18471060dd720
David Binetti <dbinetti@gmail.com>
2017-04-22 05:50:18 -0700

    Scoring manager cleanup

--- app/controllers/dashboard/scoring-manager/round/actions.js
+++ app/controllers/dashboard/scoring-manager/round/actions.js
@@ -1,37 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  // store: Ember.inject.service(),
-  // flashMessage: Ember.get(this, 'flashMessages'),
-  // actions: {
-  //   openSession() {
-  //     this.model.open()
-  //     .then(response => {
-  //       this.store.pushPayload('session', response);
-  //     })
-  //     .catch(() => {
-  //       this.get('flashMessages').danger("Error" );
-  //     });
-  //   },
-  //   closeSession() {
-  //     this.model.close()
-  //     .then(response => {
-  //       this.store.pushPayload('session', response);
-  //     })
-  //     .catch(() => {
-  //       this.get('flashMessages').danger("Error" );
-  //     });
-  //   },
-  //   validateCurrent() {
-  //     this.model.get('current').then(current => {
-  //       current.validate()
-  //       .then(response => {
-  //         this.store.pushPayload('round', response);
-  //       })
-  //       .catch(() => {
-  //         this.get('flashMessages').danger("Error" );
-  //       });
-  //     });
-  //   },
-  // }
 });
