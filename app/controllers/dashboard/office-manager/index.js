9f5baa113da5cb11ff66b7b3c32cbe3edd76903a
David Binetti <dbinetti@gmail.com>
2017-04-22 15:14:19 -0700

    De-cruft

--- app/controllers/dashboard/office-manager/index.js
+++ app/controllers/dashboard/office-manager/index.js
@@ -5,24 +5,6 @@ export default Ember.Controller.extend({
     'kind',
   ],
   kind: null,
-  // kind: Ember.computed(
-  //   'kindOut',
-  //   function(){
-  //     let k = this.get('kindOut');
-  //     console.log(k);
-  //     let map = {
-  //       'Organization': 1,
-  //       'District': 11,
-  //       'Noncompetitive': 12,
-  //       'Affiliate': 13,
-  //       'Division': 21,
-  //       'Quartet': 31,
-  //       'Chorus': 32,
-  //       'Very Large Quartet': 33,
-  //     };
-  //     return map[k];
-  //   }
-  // ),
   sortProperties: [
     'name',
     'kind',
@@ -32,16 +14,6 @@ export default Ember.Controller.extend({
     'model',
     'sortProperties'
   ),
-  // kindOptions: [
-  //   'Organization',
-  //   'District',
-  //   'Noncompetitive',
-  //   'Affiliate',
-  //   'Division',
-  //   'Quartet',
-  //   'Chorus',
-  //   'Very Large Quartet',
-  // ],
   kindOptions: [
     1,11,32
   ],
