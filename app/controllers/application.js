37feb01b6bc286b66537e9d8d5ebea355908ef9d
David Binetti <dbinetti@gmail.com>
2017-04-26 13:33:15 -0700

    Enable passwordless login

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -8,28 +8,34 @@ export default Ember.Controller.extend({
     },
     login () {
       const lockOptions = {
-        allowedConnections: [
-          'Default',
-        ],
         autoclose: true,
-        allowLogin: true,
-        allowSignUp: false,
+        icon: 'https://s3-us-west-1.amazonaws.com/barberscore/static/app/bhs_logo.png',
+        closeable: true,
+        focusInput: true,
+        gravatar: false,
+        primaryColor: '#337ab7',
         rememberLastLogin: true,
-        theme: {
-          logo: 'https://s3-us-west-1.amazonaws.com/barberscore/static/app/bhs_logo.png',
-          primaryColor: '#337ab7'
-        },
-        languageDictionary: {
-          title: "Barberscore"
-        },
-        auth: {
-          redirect: true,
-          params: {
-            scope: 'openid profile',
+        dict: {
+          title: "Barberscore",
+          email: {
+            headerText: "Enter your email address<br>registered with the BHS.",
+            footerText: "(If you aren't currently registered with the BHS, or can't remember your email, please contact <a href='mailto:support@barberscore.com'>support@barberscore.com</a> for assistance.)"
+          },
+          emailSent: {
+            success:  "We sent an email to <br>{email}<br>with a link to login."
+          },
+          error: {
+            passwordless: {
+              "bad.connection":  "We're sorry, we can't log you in because that email is not registered with the BHS.  Try again with a different email address or contact support@barberscore.com for assistance.",
+            }
           }
-        }
+        },
       };
-      this.get('session').authenticate('authenticator:auth0-lock', lockOptions);
+      this.get('session').authenticate(
+        'authenticator:auth0-lock-passwordless',
+        'magiclink',
+        lockOptions
+      );
     },
   }
 });
