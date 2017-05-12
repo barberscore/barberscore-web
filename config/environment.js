db884d79cf5a5ea7405ecc8c9162d76b4362d2d2
David Binetti <dbinetti@gmail.com>
2017-05-11 21:33:14 -0700

    Update config

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'barberscore-ember',
+    modulePrefix: 'barberscore-web',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
