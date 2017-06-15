b5dd05d881f2659e602c34dde79242d4af8f5598
David Binetti <dbinetti@gmail.com>
2017-06-15 13:59:14 -0700

    Resort Draw

--- app/controllers/dashboard/scoring-manager/round/appearances/index.js
+++ app/controllers/dashboard/scoring-manager/round/appearances/index.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   sortedAppearancesProperties: [
-    'slotNum',
+    'num',
   ],
   sortedAppearances: Ember.computed.sort(
     'model.appearances',
