4b0f8676b57537a11b9957c2cb5a6587729cf928
David Binetti <dbinetti@gmail.com>
2017-06-17 18:34:17 -0700

    Previous/Next on Session manager

--- app/controllers/dashboard/session-manager/session.js
+++ app/controllers/dashboard/session-manager/session.js
@@ -34,4 +34,33 @@ export default Ember.Controller.extend({
       this.get('flashMessages').danger("Verification Failed; Please check your entry.");
     }
   }).drop(),
+  sortedItemsSortProperties: [
+    'nomen',
+  ],
+  sortedItems: Ember.computed.sort(
+    'model.convention.sessions',
+    'sortedItemsSortProperties'
+  ),
+  isPrevDisabled: Ember.computed(
+    'model',
+    'sortedItems', function() {
+    return this.model == this.get('sortedItems.firstObject');
+  }),
+  isNextDisabled: Ember.computed(
+    'model',
+    'sortedItems', function() {
+    return this.model == this.get('sortedItems.lastObject');
+  }),
+  actions: {
+    previousItem(cursor) {
+      let nowCur = this.get('sortedItems').indexOf(cursor);
+      let newCur = this.get('sortedItems').objectAt(nowCur-1);
+      this.transitionToRoute('dashboard.session-manager.session.details', newCur);
+    },
+    nextItem(cursor) {
+      let nowCur = this.get('sortedItems').indexOf(cursor);
+      let newCur = this.get('sortedItems').objectAt(nowCur+1);
+      this.transitionToRoute('dashboard.session-manager.session.details', newCur);
+    },
+  }
 });
