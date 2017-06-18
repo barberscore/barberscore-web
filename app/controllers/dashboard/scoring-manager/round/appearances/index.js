c4692581a9dda0e299f8aee8730b9d13b5f57909
David Binetti <dbinetti@gmail.com>
2017-06-18 10:13:23 -0700

    Add sortable appearances

--- app/controllers/dashboard/scoring-manager/round/appearances/index.js
+++ app/controllers/dashboard/scoring-manager/round/appearances/index.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 // import { task, timeout } from 'ember-concurrency';
 
 export default Ember.Controller.extend({
+  flashMessages: Ember.inject.service(),
+  isSorting: false,
   sortedAppearancesProperties: [
     'num',
   ],
@@ -9,4 +11,16 @@ export default Ember.Controller.extend({
     'model.appearances',
     'sortedAppearancesProperties'
   ),
+  actions: {
+    reorderItems(itemModels) {
+      itemModels.forEach(function(item, index) {
+        item.set('num', index+1);
+      });
+    },
+    saveOrder() {
+      this.get('sortedAppearances').invoke('save');
+      this.set('isSorting', false);
+      this.get('flashMessages').success('Success');
+    }
+  }
 });
