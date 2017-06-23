676939dbade720e31ccd01c86bef73aecd354dae
David Binetti <dbinetti@gmail.com>
2017-06-23 14:07:23 -0700

    Chart object view

--- app/controllers/dashboard/chart-manager/chart.js
+++ app/controllers/dashboard/chart-manager/chart.js
@@ -1,23 +1,73 @@
 import Ember from 'ember';
+import { task, timeout } from 'ember-concurrency';
 
 export default Ember.Controller.extend({
-  isEditing: false,
-  isDisabled: Ember.computed.not('isEditing'),
+  currentUser: Ember.inject.service(),
+  store: Ember.inject.service(),
+  chartManager: Ember.inject.controller('dashboard.chart-manager'),
   flashMessages: Ember.inject.service(),
-  actions: {
-    editChart() {
-      this.set('isEditing', true);
-    },
-    cancelChart() {
-      this.model.rollbackAttributes();
-      this.set('isEditing', false);
-    },
-    saveChart() {
-      this.model.save()
-      .then(() => {
-        this.set('isEditing', false);
-        this.get('flashMessages').success('Saved');
+  isDisabled: Ember.computed.not(
+    'model.permissions.write'
+  ),
+  autosave: task(function* (property, value){
+    this.get('model').set(property, value);
+    yield timeout(1000);
+    try {
+      let model = yield this.get('model').save();
+      this.get('flashMessages').success("Saved");
+    } catch(e) {
+      this.get('flashMessages').danger("Could not save; possible duplicate or empty fields!");
+    }
+  }).restartable(),
+  activateChart: task(function *() {
+    try {
+      let chart = yield this.model.activate({
+        'by': this.get('currentUser.user.id')
       });
+      this.store.pushPayload('chart', chart);
+      this.get('flashMessages').success("Activated!");
+    } catch(e) {
+      this.get('flashMessages').danger("Please check that all fields are entered!");
+    }
+  }).drop(),
+  dectivateChart: task(function *() {
+    try {
+      let chart = yield this.model.deactivate({
+        'by': this.get('currentUser.user.id')
+      });
+      this.store.pushPayload('chart', chart);
+      this.get('flashMessages').success("Deactivated!");
+    } catch(e) {
+      this.get('flashMessages').danger("Please check that all fields are entered!");
+    }
+  }).drop(),
+  sortedItemsSortProperties: [
+    'statusSort',
+  ],
+  sortedItems: Ember.computed.sort(
+    'chartManager.sortedItems',
+    'sortedItemsSortProperties'
+  ),
+  isPrevDisabled: Ember.computed(
+    'model',
+    'sortedItems', function() {
+    return this.model == this.get('sortedItems.firstObject');
+  }),
+  isNextDisabled: Ember.computed(
+    'model',
+    'sortedItems', function() {
+    return this.model == this.get('sortedItems.lastObject');
+  }),
+  actions: {
+    previousItem(cursor) {
+      let nowCur = this.get('sortedItems').indexOf(cursor);
+      let newCur = this.get('sortedItems').objectAt(nowCur-1);
+      this.transitionToRoute('dashboard.chart-manager.chart', newCur);
     },
-  },
-});
+    nextItem(cursor) {
+      let nowCur = this.get('sortedItems').indexOf(cursor);
+      let newCur = this.get('sortedItems').objectAt(nowCur+1);
+      this.transitionToRoute('dashboard.chart-manager.chart', newCur);
+    },
+  }
+  });
