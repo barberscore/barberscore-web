64a81dac5e102860250ff6175fe18a34251410f9
David Binetti <dbinetti@gmail.com>
2017-06-23 14:01:47 -0700

    Nested outlet for Chart Manager

---
+++ app/controllers/dashboard/chart-manager.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+import { task, timeout } from 'ember-concurrency';
+
+export default Ember.Controller.extend({
+  flashMessages: Ember.inject.service(),
+  openModal: false,
+  sortProperties: [
+    'statusSort',
+    'nomen',
+  ],
+  sortedItems: Ember.computed.sort(
+    'model',
+    'sortProperties'
+  ),
+  searchChart: task(function* (term){
+    yield timeout(600);
+    return this.get('store').query('chart', {
+      'nomen__icontains': term,
+      'page_size': 1000
+      })
+      .then((data) => data);
+  }),
+  actions: {
+    transitionChart(selected) {
+      this.transitionToRoute('dashboard.chart-manager.chart', selected);
+    },
+  }
+});
--- app/routes/dashboard/chart-manager.js
+++ app/routes/dashboard/chart-manager.js
@@ -3,8 +3,11 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, RouteMixin, {
-  perPage: 100,
+  perPage: 20,
   model(params) {
+    params = {
+      'status__gte': 0
+    }
     params.paramMapping = {
       page: "page",
       perPage: "page_size",
