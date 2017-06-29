d67e9f766ddcee4caf31691718406edf66ca9665
David Binetti <dbinetti@gmail.com>
2017-06-29 14:40:26 -0700

    Sorting for Groups

--- app/controllers/dashboard/group-manager/index.js
+++ app/controllers/dashboard/group-manager/index.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  sortedGroupsProperties: [
+    'nomen',
+  ],
+  sortedGroups: Ember.computed.sort(
+    'model',
+    'sortedGroupsProperties'
+  ),
 });
