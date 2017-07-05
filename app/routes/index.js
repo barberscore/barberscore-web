90175ab216bda8ae6697af9a14c019fc700ac4d0
David Binetti <dbinetti@gmail.com>
2017-07-05 14:22:22 -0700

    Open links to current rounds

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 export default Ember.Route.extend(UnauthenticatedRouteMixin, {
+  model() {
+    return this.get('store').query('round', {
+      'status__lt': 50,
+    });
+  }
 });
