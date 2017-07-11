44f22b8ae3fe7b084b65fbbd2a86de658cf34363
David Binetti <dbinetti@gmail.com>
2017-07-11 11:32:10 -0700

    Linting

--- app/controllers/dashboard/award-manager/award.js
+++ app/controllers/dashboard/award-manager/award.js
@@ -1,48 +1,4 @@
 import Ember from 'ember';
-import { task } from 'ember-concurrency';
 
 export default Ember.Controller.extend({
-  currentUser: Ember.inject.service(),
-  store: Ember.inject.service(),
-  flashMessages: Ember.inject.service(),
-  // openSession: task(function *() {
-  //   let session = yield this.model.open({
-  //     'by': this.get('currentUser.user.id')
-  //   });
-  //   this.store.pushPayload('session', session);
-  //   this.get('flashMessages').success("Opened!");
-  // }).drop(),
-  // closeSession: task(function *() {
-  //   try {
-  //     let session = yield this.model.close({
-  //       'by': this.get('currentUser.user.id')
-  //     });
-  //     this.store.pushPayload('session', session);
-  //     this.get('flashMessages').success("Closed!");
-  //   } catch(e) {
-  //     this.get('flashMessages').danger("Closing Failed; Please check your entry.");
-  //   }
-  // }).drop(),
-  // verifySession: task(function *() {
-  //   try {
-  //     let session = yield this.model.verify({
-  //       'by': this.get('currentUser.user.id')
-  //     });
-  //     this.store.pushPayload('session', session);
-  //     this.get('flashMessages').success("Verified!");
-  //   } catch(e) {
-  //     this.get('flashMessages').danger("Verification Failed; Please check your entry.");
-  //   }
-  // }).drop(),
-  // startSession: task(function *() {
-  //   try {
-  //     let session = yield this.model.start({
-  //       'by': this.get('currentUser.user.id')
-  //     });
-  //     this.store.pushPayload('session', session);
-  //     this.get('flashMessages').success("Started!");
-  //   } catch(e) {
-  //     this.get('flashMessages').danger("Session Start Failed; Please check your entry.");
-  //   }
-  // }).drop(),
 });
