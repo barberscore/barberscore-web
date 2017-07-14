9799ab963a1f096aaa153b09eae2bed99609bde2
David Binetti <dbinetti@gmail.com>
2017-07-13 17:45:12 -0700

    Add currentUser

--- app/controllers/dashboard/convention-manager/convention/sessions/session.js
+++ app/controllers/dashboard/convention-manager/convention/sessions/session.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import { task } from 'ember-concurrency';
 
 export default Ember.Controller.extend({
+  currentUser: Ember.inject.service(),
   flashMessages: Ember.inject.service(),
   awardSortProperties: [
     'entityKindSort',
