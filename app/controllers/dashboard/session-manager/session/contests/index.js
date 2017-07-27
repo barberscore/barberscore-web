30e8bf36854820849eac35ff6a41d030970bc368
David Binetti <dbinetti@gmail.com>
2017-07-26 22:44:24 -0700

    Use Contests with split UI

--- app/controllers/dashboard/session-manager/session/contests/index.js
+++ app/controllers/dashboard/session-manager/session/contests/index.js
@@ -43,6 +43,25 @@ export default Ember.Controller.extend({
       .then((data) => data);
   }),
   flashMessages: Ember.inject.service(),
+  awardSortProperties: [
+    'organizationKindSort',
+    'isQualifier',
+    'isPrimary:desc',
+    'ageSort',
+    'name',
+  ],
+  filteredAwards: Ember.computed(
+    'model.convention.organization.awards.@each.{kind,season}',
+    'model.kind',
+    'model.convention.season',
+    function() {
+      return this.get('model.convention.organization.awards').filterBy('kind', this.get('model.kind')).filterBy('season', this.get('model.convention.season'));
+    }
+  ),
+  sortedAwards: Ember.computed.sort(
+    'filteredAwards',
+    'awardSortProperties',
+  ),
   actions: {
     sortBy(contestSortProperties) {
       this.set('contestSortProperties', [contestSortProperties]);
