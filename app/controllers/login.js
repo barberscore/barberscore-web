0d2909d9db3f4bed1ae5e59a82d7cdd951a17c80
David Binetti <dbinetti@gmail.com>
2017-08-01 16:43:38 -0700

    Switch to BHS customer service email

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -17,14 +17,14 @@ export default Ember.Controller.extend({
           title: "Barberscore",
           email: {
             headerText: "Enter your email address<br>registered with the BHS.",
-            footerText: "(If you aren't currently registered with the BHS, or can't remember your email, please contact <a href='mailto:support@barberscore.com'>support@barberscore.com</a> for assistance.)"
+            footerText: "(If you aren't currently registered with the BHS, or can't remember your email, please contact <a href='mailto:customerservice@barbershop.org'>customerservice@barbershop.org</a> for assistance.)"
           },
           emailSent: {
             success:  "Check your email for the log in link.  PLEASE NOTE: It may take up to a minute for the email to be sent, so please be patient."
           },
           error: {
             passwordless: {
-              "bad.connection":  "We're sorry, we can't log you in because that email is not registered with the BHS.  Try again with a different email address or contact support@barberscore.com for assistance.",
+              "bad.connection":  "We're sorry, we can't log you in because that email is not registered with the BHS.  Try again with a different email address or contact customerservice@barbershop.org for assistance.",
             }
           }
         },
