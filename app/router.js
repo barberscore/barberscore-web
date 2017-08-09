ec366be6fce76fdec2f6f6cc8f9978d9f12223ac
David Binetti <dbinetti@gmail.com>
2017-08-09 03:55:43 -0400

    Remove note

--- app/router.js
+++ app/router.js
@@ -131,7 +131,6 @@ Router.map(function() {
     });
   });
   this.route('about');
-  this.route('note');
   this.route('tutorials');
   this.route('faq');
   this.route('404', { path: '/*wildcard' });
