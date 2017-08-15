1610026e0d27f7be18e2894a848874fcde89f76b
David Binetti <dbinetti@gmail.com>
2017-08-15 05:49:51 -0700

    Potential login loop fix

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,18 +1,21 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth-auth0/mixins/application-route-mixin';
 
+const { service } = Ember.inject;
+
 export default Ember.Route.extend(ApplicationRouteMixin, {
-  currentUser: Ember.inject.service('current-user'),
-  session: Ember.inject.service(),
+  currentUser: service(),
+
   beforeModel() {
-    this._super(...arguments);
-    return this._loadCurrentUser();
+    this._super(...arguments).then(() => this._loadCurrentUser());
   },
+
   sessionAuthenticated() {
     this._super(...arguments);
-    this._loadCurrentUser().catch(() => this.get('session').invalidate());
+    this._loadCurrentUser();
   },
+
   _loadCurrentUser() {
-    return this.get('currentUser').load();
-  },
+    return this.get('currentUser').load().catch(() => this.get('session').invalidate());
+  }
 });
