cb21298aa4921560ec9a69a349b37f60efea4fb5
David Binetti <dbinetti@gmail.com>
2017-08-23 14:29:01 -0700

    Make user on Group Manager thennable

--- app/routes/dashboard/group-manager/index.js
+++ app/routes/dashboard/group-manager/index.js
@@ -4,8 +4,9 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   currentUser: Ember.inject.service('current-user'),
   model: function() {
+    let user = this.get('currentUser.user.id').then((data) => data);
     return this.get('store').query('member', {
-      'person__user': this.get('currentUser.user.id'),
+      'person__user': user,
       'is_admin': true,
       'status__gte': '0',
       'page_size': 100,
