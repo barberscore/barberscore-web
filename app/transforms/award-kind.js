bef1ff9e9fb90445e816b1560e31f733a85e6953
David Binetti <dbinetti@gmail.com>
2017-08-24 08:54:48 -0700

    Strip kind

--- app/transforms/award-kind.js
+++ app/transforms/award-kind.js
@@ -4,8 +4,6 @@ export default DS.Transform.extend({
   deserialize: function(serialized) {
     var map = {
       32: 'Chorus',
-      33: 'Very Large Quartet',
-      34: 'Mixed Group',
       41: 'Quartet',
     };
     return map[serialized];
@@ -14,8 +12,6 @@ export default DS.Transform.extend({
   serialize: function(deserialized) {
     var map = {
       'Chorus': 32,
-      'Very Large Quartet': 33,
-      'Mixed Group': 34,
       'Quartet': 41,
     };
     return map[deserialized];
