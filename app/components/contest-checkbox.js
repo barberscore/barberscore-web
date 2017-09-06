a5d99f8b1d58ef47c907d3eb8e9ac09059b6b9d0
David Binetti <dbinetti@gmail.com>
2017-09-06 12:32:20 -0700

    Fix empty contestants array for session manager

--- app/components/contest-checkbox.js
+++ app/components/contest-checkbox.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
-import { task } from 'ember-concurrency';
+import {
+  task
+} from 'ember-concurrency';
 
 export default Ember.Component.extend({
   store: Ember.inject.service(),
@@ -11,27 +13,28 @@ export default Ember.Component.extend({
   isSelected: Ember.computed(
     'award',
     'contests.@each.award',
-    function(){
+    function () {
       let mapped = this.get('contests').mapBy('award.id');
       if (mapped.includes(this.get('award.id'))) {
         return true;
       } else {
         return false;
       }
-  }),
-  toggleAward: task(function* (property, value){
+    }),
+  toggleAward: task(function* (property, value) {
     if (value) {
       let newContest = this.get('store').createRecord('contest', {
         award: this.get('award'),
         kind: this.get('award.kind'),
         session: this.get('model'),
+        contestants: [],
         isQualifier: false,
       });
       yield newContest.save();
       this.get('flashMessages').success("Saved");
     } else {
       let contest = this.get('model.contests').findBy('award.id', this.get('award.id'));
-      contest.destroyRecord().then(()=> this.get('flashMessages').success("Saved"));
+      contest.destroyRecord().then(() => this.get('flashMessages').success("Saved"));
     }
   }).restartable(),
 });
