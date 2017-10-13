d52810a3470377d22c00ac35c3b3ad44dda740ba
David Binetti <dbinetti@gmail.com>
2017-10-13 12:52:17 -0700

    Push fix for uglify

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -34,6 +34,7 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('node_modules/popper.js/dist/umd/popper.js.map', { destDir: 'assets' });
 
   return app.toTree();
 };
