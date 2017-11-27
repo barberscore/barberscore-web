fea53f4e5cb9ce6b1ab1651f8dcca06e963c8501
David Binetti <dbinetti@gmail.com>
2017-11-27 14:24:48 -0800

    Update FSM for Round

--- app/controllers/dashboard/scoring-manager/round.js
+++ app/controllers/dashboard/scoring-manager/round.js
@@ -5,13 +5,6 @@ import { task } from 'ember-concurrency';
 export default Controller.extend({
   store: service(),
   flashMessages: service(),
-  verifyRound: task(function *() {
-    let round = yield this.model.verify({
-      'by': this.get('currentUser.user.id')
-    });
-    this.store.pushPayload('round', round);
-    this.get('flashMessages').success("Verified!");
-  }).drop(),
   startRound: task(function *() {
     let round = yield this.model.start({
       'by': this.get('currentUser.user.id')
@@ -19,18 +12,18 @@ export default Controller.extend({
     this.store.pushPayload('round', round);
     this.get('flashMessages').success("Started!");
   }).drop(),
+  reviewRound: task(function *() {
+    let round = yield this.model.review({
+      'by': this.get('currentUser.user.id')
+    });
+    this.store.pushPayload('round', round);
+    this.get('flashMessages').success("Reviewed!");
+  }).drop(),
   finishRound: task(function *() {
     let round = yield this.model.finish({
       'by': this.get('currentUser.user.id')
     });
     this.store.pushPayload('round', round);
     this.get('flashMessages').success("Finished!");
-  }).drop(),
-  announceRound: task(function *() {
-    let round = yield this.model.announce({
-      'by': this.get('currentUser.user.id')
-    });
-    this.store.pushPayload('round', round);
-    this.get('flashMessages').success("Announced!");
   }).drop(),
 });
