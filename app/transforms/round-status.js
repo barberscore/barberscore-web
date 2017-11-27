d23564fe9fb8e83a7345b7bf68b46ff035f9f9f5
David Binetti <dbinetti@gmail.com>
2017-11-27 14:05:51 -0800

    Update Round Status

--- app/transforms/round-status.js
+++ app/transforms/round-status.js
@@ -4,15 +4,9 @@ export default DS.Transform.extend({
   deserialize: function(serialized) {
     var map = {
       0: 'New',
-      2: 'Listed',
-      4: 'Opened',
-      8: 'Closed',
-      10: 'Verified',
-      15: 'Prepared',
       20: 'Started',
+      25: 'Reviewed',
       30: 'Finished',
-      50: 'Announced',
-      95: 'Archived',
     };
     return map[serialized];
   },
@@ -20,15 +14,9 @@ export default DS.Transform.extend({
   serialize: function(deserialized) {
     var map = {
       'New': 0,
-      'Listed': 2,
-      'Opened': 4,
-      'Closed': 8,
-      'Verified': 10,
-      'Prepared': 15,
       'Started': 20,
+      'Reviewed': 25,
       'Finished': 30,
-      'Announced': 50,
-      'Archived': 95,
 };
     return map[deserialized];
   }
