ee03994fd4793bbec5f1783f7b44bf079c7dbfe3
David Binetti <dbinetti@gmail.com>
2017-12-11 09:54:16 -0800

    Update Assignment kind, status transforms

--- app/transforms/assignment-status.js
+++ app/transforms/assignment-status.js
@@ -4,8 +4,11 @@ export default DS.Transform.extend({
   deserialize: function(serialized) {
     var map = {
       '-10': 'Inactive',
-      0: 'New',
-      10: 'Active',
+      '0': 'New',
+      '10': 'Active',
+      '20': 'Scheduled',
+      '30': 'Withdrawn',
+      '40': 'Confirmed',
     };
     return map[serialized];
   },
@@ -13,8 +16,11 @@ export default DS.Transform.extend({
   serialize: function(deserialized) {
     var map = {
       'Inactive': '-10',
-      'New': 0,
-      'Active': 10,
+      'New': '0',
+      'Active': '10',
+      'Scheduled': '20',
+      'Withdrawn': '30',
+      'Confirmed': '40',
     };
     return map[deserialized];
   }
