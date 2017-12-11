b84163b11173e5c3056bb258de7e3dd0f4ee48c6
David Binetti <dbinetti@gmail.com>
2017-12-11 15:24:31 -0800

    Align assignments to state

--- app/transforms/assignment-status.js
+++ app/transforms/assignment-status.js
@@ -3,24 +3,18 @@ import DS from 'ember-data';
 export default DS.Transform.extend({
   deserialize: function(serialized) {
     var map = {
-      '-10': 'Inactive',
+      '-10': 'Withdrawn',
       '0': 'New',
-      '10': 'Active',
-      '20': 'Scheduled',
-      '30': 'Withdrawn',
-      '40': 'Confirmed',
+      '10': 'Confirmed',
     };
     return map[serialized];
   },
 
   serialize: function(deserialized) {
     var map = {
-      'Inactive': '-10',
+      'Withdrawn': '-10',
       'New': '0',
       'Active': '10',
-      'Scheduled': '20',
-      'Withdrawn': '30',
-      'Confirmed': '40',
     };
     return map[deserialized];
   }
