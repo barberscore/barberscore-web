c1f442241b87e1932c982464c2e0652f82a0899a
David Binetti <dbinetti@gmail.com>
2018-02-09 22:08:36 -0800

    Contests tab as component

--- app/models/contest.js
+++ app/models/contest.js
@@ -1,4 +1,4 @@
-import { alias, filterBy } from '@ember/object/computed';
+import { alias, filterBy, equal } from '@ember/object/computed';
 import Model from 'ember-data/model';
 import DS from 'ember-data';
 import { memberAction } from 'ember-api-actions';
@@ -37,6 +37,10 @@ export default Model.extend({
     'includedContestants.length'
   ),
 
+  isIncluded: equal(
+    'status',
+    'Included',
+  ),
 
   organizationKindSort: alias('award.organization.kindSort'),
   awardQualifier: alias('award.isQualifier'),
