9ac151cc2e517cce8f60567b9d6a36a60f683485
David Binetti <dbinetti@gmail.com>
2018-02-21 18:31:24 -0800

    Add groupkind filter

--- app/transforms/group-kind.js
+++ app/transforms/group-kind.js
@@ -3,6 +3,12 @@ import DS from 'ember-data';
 export default DS.Transform.extend({
   deserialize: function(serialized) {
     var map = {
+      1: 'International',
+      11: 'District',
+      12: 'Non-Competitive',
+      13: 'Affiliate',
+      21: 'Division',
+      30: 'Chapter',
       32: 'Chorus',
       41: 'Quartet',
     };
@@ -11,6 +17,12 @@ export default DS.Transform.extend({
 
   serialize: function(deserialized) {
     var map = {
+      'International': 1,
+      'District': 11,
+      'Non-Competitive': 12,
+      'Affiliate': 13,
+      'Division': 21,
+      'Chapter': 30,
       'Chorus': 32,
       'Quartet': 41,
     };
