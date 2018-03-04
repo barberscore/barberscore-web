9b90aca6f94d0dfbd97a390c2f0e44c1c1553f05
David Binetti <dbinetti@gmail.com>
2018-03-04 15:46:16 -0800

    De-cruft

--- app/controllers/dashboard/group-manager/group/repertories/index.js
+++
@@ -1,9 +0,0 @@
-import Controller from '@ember/controller';
-
-export default Controller.extend({
-  actions: {
-    createRepertory() {
-      this.transitionToRoute('dashboard.group-manager.group.repertories.new');
-    },
-  },
-});
