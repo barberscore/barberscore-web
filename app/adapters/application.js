4092fa469058ff300c8cb3211afde5a0878ad99c
David Binetti <dbinetti@gmail.com>
2018-03-14 14:44:15 -0700

    Revert "Add data adapter hsot"
    This reverts commit 20a22994a4b619ec58e123de3eb7c4de9f4af786.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,7 +3,7 @@ import ENV from '../config/environment';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
-  host: ENV.APP.API_HOST_DIRECT,
+  host: ENV.APP.API_HOST,
   namespace: ENV.APP.API_NAMESPACE,
   coalesceFindRequests: true,
   authorizer: 'authorizer:jwt',
