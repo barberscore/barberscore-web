97ad892fd20e276c2b09428615bdd29f738b4b99
David Binetti <dbinetti@gmail.com>
2018-06-07 10:16:29 -0700

    Add `Built` to round status

--- app/transforms/round-status.js
+++ app/transforms/round-status.js
@@ -4,6 +4,7 @@ export default DS.Transform.extend({
   deserialize: function(serialized) {
     var map = {
       0: 'New',
+      10: 'Built',
       20: 'Started',
       25: 'Reviewed',
       30: 'Finished',
@@ -14,6 +15,7 @@ export default DS.Transform.extend({
   serialize: function(deserialized) {
     var map = {
       'New': 0,
+      'Built': 10,
       'Started': 20,
       'Reviewed': 25,
       'Finished': 30,
