f19e13846faeb6806d796ab55db6cd79971a1059
David Binetti <dbinetti@gmail.com>
2018-07-03 11:28:52 -0700

    Change panelist sort order

--- app/components/appearance-panelists.js
+++ app/components/appearance-panelists.js
@@ -7,6 +7,8 @@ export default Component.extend({
   sortedPanelistsProperties: [
     'category',
     'kind',
+    'person__last_name',
+    'person__first_name',
   ],
   filteredPanelists: filterBy(
     'model.round.panelists',
