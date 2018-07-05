a5c26ee6588afb693e862d94028b27a21a9d6a3d
David Binetti <dbinetti@gmail.com>
2018-07-05 12:23:58 -0400

    Rank by score

--- app/components/round-competitors.js
+++ app/components/round-competitors.js
@@ -25,8 +25,7 @@ export default Component.extend({
   sortedCompetitorsProperties: [
     'statusSort',
     'isRanked:desc',
-    'totRank',
-    'totPoints:desc',
+    'totScore:desc',
     'groupName',
   ],
   sortedCompetitors: sort(
