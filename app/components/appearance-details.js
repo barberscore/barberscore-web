481122462efcf0a2e1bddb59b050e6637c1fca7c
David Binetti <dbinetti@gmail.com>
2018-07-05 23:05:38 -0400

    MOS Support

---
+++ app/components/appearance-details.js
@@ -0,0 +1,19 @@
+import Component from '@ember/component';
+import { task, timeout } from 'ember-concurrency';
+import { inject as service } from '@ember/service';
+
+export default Component.extend({
+  flashMessages: service(),
+  autosave: task(function* (value){
+    this.get('model').set('mos', value);
+    yield timeout(1000);
+    try {
+      yield this.get('model').save();
+      this.get('flashMessages').success("Saved");
+    } catch(e) {
+      e.errors.forEach((error) => {
+        this.get('flashMessages').danger(error.detail);
+      })
+    }
+  }).restartable(),
+});
