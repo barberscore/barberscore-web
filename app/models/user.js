1383aa420b0e55ff72755f4d191e84a50115c6db
David Binetti <dbinetti@gmail.com>
2018-07-19 10:08:38 -0700

    Add manager perms to User model

--- app/models/user.js
+++ app/models/user.js
@@ -10,5 +10,14 @@ export default DS.Model.extend({
   isDisabled: not(
     'permissions.write'
   ),
-
+  isConventionManager: DS.attr('boolean'),
+  isSessionManager: DS.attr('boolean'),
+  isRoundManager: DS.attr('boolean'),
+  isScoringManager: DS.attr('boolean'),
+  isGroupManager: DS.attr('boolean'),
+  isPersonManager: DS.attr('boolean'),
+  isAwardManager: DS.attr('boolean'),
+  isOfficerManager: DS.attr('boolean'),
+  isChartManager: DS.attr('boolean'),
+  isAssignmentManager: DS.attr('boolean'),
 });
