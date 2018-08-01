a8226655b82379fcb293320ee761d2c03e57f463
David Binetti <dbinetti@gmail.com>
2018-08-01 10:17:33 -0700

    Update dashboard

--- app/controllers/dashboard/index.js
+++ app/controllers/dashboard/index.js
@@ -1,8 +1,24 @@
 import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
+import { not, or } from '@ember/object/computed';
 
 export default Controller.extend({
   currentUser: service(),
+  groupManager: or(
+    'currentUser.user.isGroupManager',
+  ),
+  groupDisabled: not(
+    'groupManager',
+  ),
+  assignmentManager: or(
+    'currentUser.user.isConventionManager',
+    'currentUser.user.isSessionManager',
+    'currentUser.user.isRoundManager',
+    'currentUser.user.isScoringManager',
+  ),
+  assignmentDisabled: not(
+    'assignmentManager',
+  )
 });
 
 
