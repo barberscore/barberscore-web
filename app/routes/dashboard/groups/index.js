9e2e3341d42eab7979b3e4f1b1a91e609c213d85
David Binetti <dbinetti@gmail.com>
2018-08-03 10:18:27 -0700

    Fix Convention and Group access points

--- app/routes/dashboard/groups/index.js
+++ app/routes/dashboard/groups/index.js
@@ -1,13 +1,9 @@
-import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Route.extend(AuthenticatedRouteMixin, {
-  currentUser: service('current-user'),
   model() {
     return this.get('store').query('group', {
-      'members__person__user': this.get('currentUser.user.id'),
-      'members__status': 10,
       'status': 10,
       'kind__gt': 21,
     });
