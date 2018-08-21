2535329b05bdb20b38fab20c9202260ff83c6476
David Binetti <dbinetti@gmail.com>
2018-08-21 15:53:01 -0700

    Add status to user model

--- app/models/user.js
+++ app/models/user.js
@@ -2,6 +2,7 @@ import { not } from '@ember/object/computed';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  status: DS.attr('user-status'),
   username: DS.attr('string'),
   isActive: DS.attr('boolean'),
   isStaff: DS.attr('boolean'),
