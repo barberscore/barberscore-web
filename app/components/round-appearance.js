746f3fb73a7163cdbb7a5c1d9c93d947311f9f89
David Binetti <dbinetti@gmail.com>
2018-09-12 08:32:05 -0700

    Add mock methad

--- app/components/round-appearance.js
+++ app/components/round-appearance.js
@@ -1,8 +1,10 @@
 import Component from '@ember/component';
 import { sort } from '@ember/object/computed';
 import { task, timeout } from 'ember-concurrency';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  store: service(),
   autosave: task(function* (property, value){
     this.get('model').set(property, value);
     yield timeout(1000);
@@ -28,5 +30,12 @@ export default Component.extend({
   sortedSongs: sort(
     'model.songs',
     'sortedSongsProperties',
-  )
+  ),
+  mockAppearance: task(function *() {
+    let appearance = yield this.model.mock({
+    });
+    yield this.get('store').pushPayload('appearance', appearance);
+    yield this.get('model.songs').invoke('reload');
+    this.get('flashMessages').success("Mocked!");
+  }).drop(),
 });
