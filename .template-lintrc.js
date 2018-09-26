25880aaf310fc8c1534e6876079a3f58d757654a
David Binetti <dbinetti@gmail.com>
2018-09-26 10:09:37 -0700

    Disable template linter

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended'
+  extends: 'recommended',
+  rules: {
+    'attribute-indentation': false,
+    'block-indentation': false,
+    'quotes': false,
+    'self-closing-void-elements': false,
+    'img-alt-attributes': false,
+    'no-quoteless-attributes': false,
+    'simple-unless': false,
+    'table-groups': false,
+    'no-unnecessary-concat': false,
+    'link-rel-noopener': false,
+  }
 };
