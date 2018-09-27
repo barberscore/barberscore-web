951dc2832eb73ea2768bf957ad7afc81150a4fcf
David Binetti <dbinetti@gmail.com>
2018-09-27 07:03:58 -0700

    Revert "WIP Bootstrap 4.1"
    This reverts commit 7c593584745f1d8a4f1da58bf6e62b549afac633.

--- app/helpers/image-width.js
+++
@@ -1,9 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export function oneIndexed(params) {
-  if (params[0]) {
-    return params[0] + 1;
-  }
-}
-
-export default helper(oneIndexed);
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,18 +4,16 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    'ember-bootstrap': {
+      'bootstrapVersion': 3,
+      'importBootstrapFont': true,
+      'importBootstrapCSS': true
+    },
     'ember-power-select': {
       theme: 'bootstrap'
     },
-
     sourcemaps: {
       enabled: true
-    },
-
-    'ember-bootstrap': {
-      'bootstrapVersion': 4,
-      'importBootstrapFont': false,
-      'importBootstrapCSS': false
     }
   });
 
