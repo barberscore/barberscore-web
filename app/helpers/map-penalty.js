d89c1d1f92663c9968d295008ed45babe819195d
David Binetti <dbinetti@gmail.com>
2019-01-14 10:07:58 -0800

    Update map-penalty.js

--- app/helpers/map-penalty.js
+++ app/helpers/map-penalty.js
@@ -3,8 +3,7 @@ import { helper } from '@ember/component/helper';
 export function fooBar(params) {
   if (params[0]) {
     let penaltyMap = {
-      10: 'Primarily Patriotic Intent',
-      20: 'Primarily Religious Intent',
+      10: 'Primarily Patriotic/Religious Intent',
       30: 'Instrumental Accompaniment',
       40: 'Chorus Exceeding 4-Part Texture',
       50: 'Sound Equipment or Electronic Enhancement',
