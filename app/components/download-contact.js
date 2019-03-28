85264ad7238ff1baa65bf9fe3656dad0c0571a07
David Binetti <dbinetti@gmail.com>
2019-03-28 09:56:45 -0700

    Fix typo

--- app/components/download-contact.js
+++ app/components/download-contact.js
@@ -5,7 +5,7 @@ import FileSaverMixin from 'ember-cli-file-saver/mixins/file-saver';
 export default Component.extend(FileSaverMixin,{
   contact: task(function *() {
     let xlsx = yield this.model.contact();
-    let fileName = `${this.model.conventionName} ${this.model.kind} Session Contacat Report`;
+    let fileName = `${this.model.conventionName} ${this.model.kind} Session Contact Report`;
     this.saveFileAs(fileName, xlsx, 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
   }).drop(),
 });
