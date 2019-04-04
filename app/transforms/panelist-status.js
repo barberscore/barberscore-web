162d39748c3711e323e68a159f0dcfd1cb578958
David Binetti <dbinetti@gmail.com>
2019-04-04 14:44:43 -0700

    Add PSA to front-end

--- app/transforms/panelist-status.js
+++ app/transforms/panelist-status.js
@@ -4,6 +4,7 @@ export default DS.Transform.extend({
   deserialize: function(serialized) {
     var map = {
       '-10': 'Inactive',
+      '-5': 'Completed',
       0: 'New',
       10: 'Active',
     };
@@ -13,6 +14,7 @@ export default DS.Transform.extend({
   serialize: function(deserialized) {
     var map = {
       'Inactive': '-10',
+      'Completed': '-5',
       'New': 0,
       'Active': 10,
     };
