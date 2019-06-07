92aae814313490bb7e6e819f4d7ed86ff17f5427
David Binetti <dbinetti@gmail.com>
2019-06-07 13:49:40 -0700

    Add smanager adapters

---
+++ app/adapters/contestant.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+import ENV from '../config/environment';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+import AdapterArrayBufferMixin from 'ember-cli-file-saver/mixins/adapter-arraybuffer-mixin';
+
+export default DS.JSONAPIAdapter.extend(AdapterArrayBufferMixin, DataAdapterMixin, {
+  host: ENV.APP.API_HOST,
+  namespace: 'smanager',
+  coalesceFindRequests: false,
+  authorizer: 'authorizer:jwt',
+});
