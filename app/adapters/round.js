f3faf4f022e711e5f53064fe6442dc7d8f09adde
David Binetti <dbinetti@gmail.com>
2019-06-09 11:51:05 -0700

    Update adapters

---
+++ app/adapters/round.js
@@ -0,0 +1,17 @@
+import DS from 'ember-data';
+import ENV from '../config/environment';
+import { isPresent } from '@ember/utils';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+import AdapterArrayBufferMixin from 'ember-cli-file-saver/mixins/adapter-arraybuffer-mixin';
+
+export default DS.JSONAPIAdapter.extend(AdapterArrayBufferMixin, DataAdapterMixin, {
+  host: ENV.APP.API_HOST,
+  namespace: 'rmanager',
+  coalesceFindRequests: false,
+  authorize(xhr) {
+    let { idToken } = this.get('session.data.authenticated');
+    if (isPresent(idToken)) {
+      xhr.setRequestHeader('Authorization', `Bearer ${idToken}`);
+    }
+  }
+});
