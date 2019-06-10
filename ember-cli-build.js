dccab12dfe1d19546de73d0782699e385b66bcf2
David Binetti <dbinetti@gmail.com>
2019-06-09 19:50:58 -0700

    Homepage, navbar and footer

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,8 +5,8 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     'ember-bootstrap': {
-      'bootstrapVersion': 3,
-      'importBootstrapFont': true,
+      'bootstrapVersion': 4,
+      'importBootstrapFont': false,
       'importBootstrapCSS': true
     },
     'ember-power-select': {
