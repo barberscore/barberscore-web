db634dc51ceaf5e37439754b389f36744acee6c2
David Binetti <dbinetti@gmail.com>
2019-06-27 08:16:23 -0700

    Modify dashboard to be 'person' based

--- app/controllers/dashboard/index.js
+++ app/controllers/dashboard/index.js
@@ -1,24 +1,11 @@
-import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
-import { not, or } from '@ember/object/computed';
+import { empty } from '@ember/object/computed';
 
 export default Controller.extend({
-  currentUser: service(),
-  groupManager: or(
-    'currentUser.user.isGroupManager',
+  membersDisabled: empty(
+    'model.members',
   ),
-  groupDisabled: not(
-    'groupManager',
+  assignmentsDisabled: empty(
+    'model.assignments',
   ),
-  assignmentManager: or(
-    'currentUser.user.isConventionManager',
-    'currentUser.user.isSessionManager',
-    'currentUser.user.isRoundManager',
-    'currentUser.user.isScoringManager',
-  ),
-  assignmentDisabled: not(
-    'assignmentManager',
-  )
 });
-
-
