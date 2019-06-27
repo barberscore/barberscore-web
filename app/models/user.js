0e0b9474b62bd78fa8a0089f1f186c78d983d98d
David Binetti <dbinetti@gmail.com>
2019-06-27 08:16:33 -0700

    Remove user link

--- app/models/user.js
+++ app/models/user.js
@@ -4,9 +4,9 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   status: DS.attr('user-status'),
   username: DS.attr('string'),
+  name: DS.attr('string'),
   isActive: DS.attr('boolean'),
   isStaff: DS.attr('boolean'),
-  person: DS.belongsTo('person', {async: true}),
   permissions: DS.attr(),
   isDisabled: not(
     'permissions.write'
