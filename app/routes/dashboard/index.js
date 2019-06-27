65be54536ab81ce6aa05eaee542f38e5ff84ea1d
David Binetti <dbinetti@gmail.com>
2019-06-27 08:16:08 -0700

    Hack to populate person on initial login

--- app/routes/dashboard/index.js
+++ app/routes/dashboard/index.js
@@ -1,23 +1,23 @@
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-// import { inject as service } from '@ember/service';
-// import RSVP from 'rsvp';
+import { inject as service } from '@ember/service';
+import { isEmpty } from '@ember/utils';
 
 export default Route.extend(AuthenticatedRouteMixin, {
-  // currentUser: service('current-user'),
-  // store: service(),
-  // model() {
-  //   return RSVP.hash({
-  //     activeOfficers:  this.get('store').query('officer', {
-  //       'person__user': this.get('currentUser.user.id'),
-  //       'status': 10,
-  //       'group__status': 10,
-  //     }),
-  //     activeAssignments:  this.get('store').query('assignment', {
-  //       'person__user': this.get('currentUser.user.id'),
-  //       'status': 10,
-  //       'convention__status': 10,
-  //     }),
-  //   });
-  // }
+  currentUser: service(),
+  session: service(),
+  store: service(),
+  model() {
+    let userId = this.get('currentUser.user.id');
+    if (isEmpty(userId)) {
+      let username = this.get('session.data.authenticated.profile.sub');
+      return this.store.query('person', {
+        'user__username': username
+      }).then(response => response.firstObject);
+    } else {
+      return this.store.query('person', {
+        'user': userId
+      }).then(response => response.firstObject);
+    }
+  },
 });
