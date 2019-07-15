a55676b04a126a12fdf8c87cab0b163d7cd59053
David Binetti <dbinetti@gmail.com>
2019-07-15 14:23:58 -0700

    Cleanup chart model

--- app/models/chart.js
+++ app/models/chart.js
@@ -5,32 +5,35 @@ import DS from 'ember-data';
 import { memberAction } from 'ember-api-actions';
 
 export default Model.extend({
-  nomen: DS.attr('string'),
   status: DS.attr('chart-status'),
   title: DS.attr('string'),
   arrangers: DS.attr('string'),
   composers: DS.attr('string'),
   lyricists: DS.attr('string'),
+  holders: DS.attr('string'),
   description: DS.attr('string'),
   notes: DS.attr('string'),
   image: DS.attr('string'),
+
+  nomen: DS.attr('string'),
   imageId: DS.attr('string'),
-  holders: DS.attr('string', {defaultValue:''}),
+
   repertories: DS.hasMany('repertory', {async: true}),
-  songs: DS.hasMany('song', {async: true}),
   permissions: DS.attr(),
 
   activate: memberAction({path: 'activate', type: 'post'}),
   deactivate: memberAction({path: 'deactivate', type: 'post'}),
+  protect: memberAction({path: 'protect', type: 'post'}),
 
   isDisabled: not(
     'permissions.write'
   ),
 
   statusOptions: [
+    'Protected',
+    'Inactive',
     'New',
     'Active',
-    'Inactive',
   ],
   statusSort: computed(
     'status',
