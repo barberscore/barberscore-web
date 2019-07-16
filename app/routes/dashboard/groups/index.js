ed7e6f5445bc1d691c57f01df3d1756d341143f2
David Binetti <dbinetti@gmail.com>
2019-07-16 10:21:17 -0700

    Filter groups

--- app/routes/dashboard/groups/index.js
+++ app/routes/dashboard/groups/index.js
@@ -1,9 +1,12 @@
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import { inject as service } from '@ember/service';
 
 export default Route.extend(AuthenticatedRouteMixin, {
+  currentUser: service(),
   model() {
     return this.store.query('group', {
+      'owners': this.currentUser.user.id,
       'status': 10,
       'kind__gt': 21,
     });
