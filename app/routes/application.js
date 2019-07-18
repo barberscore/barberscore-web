e12a5f9d01403c2443b1f56daa6666c261ea6e78
David Binetti <dbinetti@gmail.com>
2019-07-18 14:49:29 -0700

    Try to set user

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import ApplicationRouteMixin from 'ember-simple-auth-auth0/mixins/application-route-mixin';
+import Sentry from '../sentry';
 
 export default Route.extend(ApplicationRouteMixin, {
   currentUser: service(),
@@ -14,9 +15,17 @@ export default Route.extend(ApplicationRouteMixin, {
   sessionAuthenticated() {
     this._super(...arguments);
     this._loadCurrentUser();
+    this._setSentryUser();
   },
 
   _loadCurrentUser() {
     return this.currentUser.load().catch(() => this.session.invalidate());
+  },
+  _setSentryUser() {
+    return Sentry.setUser({
+        "id": this.currentUser.user.id,
+        "email": this.currentUser.user.email,
+        "username": this.currentUser.user.name,
+    });
   }
 });
