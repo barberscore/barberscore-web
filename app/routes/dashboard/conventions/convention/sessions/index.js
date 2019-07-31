1bfd6b5cb0e985241c799218bdcc34c415479995
David Binetti <dbinetti@gmail.com>
2019-07-31 06:06:14 -0700

    Add filtering for convention sessions

--- app/routes/dashboard/conventions/convention/sessions/index.js
+++ app/routes/dashboard/conventions/convention/sessions/index.js
@@ -5,10 +5,12 @@ import { inject as service } from '@ember/service';
 export default Route.extend(AuthenticatedRouteMixin, {
   currentUser: service(),
   model() {
+    let { convention_id } = this.paramsFor('dashboard.conventions.convention');
     return this.store.query('session', {
       'owners': this.currentUser.user.id,
       'status__gt': 0,
       'status__lt': 30,
+      'convention_id': convention_id,
       // 'kind__gt': 21,
     });
   },
