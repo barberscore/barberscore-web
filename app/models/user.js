8df2a220cc3bd958e48c24c44ead74eb69789247
David Binetti <dbinetti@gmail.com>
2019-08-23 13:38:52 -0700

    Remove username from user

--- app/models/user.js
+++ app/models/user.js
@@ -3,7 +3,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   status: DS.attr('user-status'),
-  username: DS.attr('string'),
   name: DS.attr('string'),
   email: DS.attr('string'),
   isActive: DS.attr('boolean'),
