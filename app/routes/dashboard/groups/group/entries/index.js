dfa11bd37ebbb9317ddc8934127a151274a76fe0
David Binetti <dbinetti@gmail.com>
2019-08-29 13:50:03 -0700

    Decruft group entries

--- app/routes/dashboard/groups/group/entries/index.js
+++ app/routes/dashboard/groups/group/entries/index.js
@@ -1,9 +1,7 @@
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import { inject as service } from '@ember/service';
 
 export default Route.extend(AuthenticatedRouteMixin, {
-  currentUser: service(),
   model() {
     let { group_id } = this.paramsFor('dashboard.groups.group');
     return this.store.query(
