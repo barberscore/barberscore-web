edaf72440fdd9e29c84d25b66c0a5b4e714d7708
David Binetti <dbinetti@gmail.com>
2019-09-01 21:24:23 -0700

    Sort by name

--- app/components/session-entries.js
+++ app/components/session-entries.js
@@ -10,7 +10,7 @@ export default Component.extend({
   algolia: service(),
   store: service(),
   sortedEntriesProperties: [
-    'groupName',
+    'name',
   ],
   sortedEntries: sort(
     'model.entries',
