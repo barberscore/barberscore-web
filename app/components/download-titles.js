0c64a42780ca483247756d0f0d9989e0e4cbbe25
David Binetti <dbinetti@gmail.com>
2019-09-25 06:20:20 -0700

    Update download-titles.js

--- app/components/download-titles.js
+++ app/components/download-titles.js
@@ -7,7 +7,7 @@ export default Component.extend(FileSaverMixin,{
   flashMessages: service(),
   titles: task(function *() {
     let pdf = yield this.model.titles();
-    let fileName = `${this.model.conventionName} ${this.model.sessionKind} ${this.model.kind} Titles Report`;
+    let fileName = `${this.model.nomen} Titles Report`;
     this.saveFileAs(fileName, pdf, 'application/pdf');
     this.flashMessages.success("Downloaded!");
   }).drop(),
