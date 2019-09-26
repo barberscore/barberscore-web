0c93fb01748faaa024afc30845ca457fed19de36
David Binetti <dbinetti@gmail.com>
2019-09-26 13:28:04 -0700

    Fungible round date

--- app/components/download-sa.js
+++ app/components/download-sa.js
@@ -7,7 +7,7 @@ export default Component.extend(FileSaverMixin,{
   flashMessages: service(),
   sa: task(function *() {
     let pdf = yield this.model.sa();
-    let fileName = `${this.model.conventionName} ${this.model.sessionKind} ${this.model.kind} SA`;
+    let fileName = `${this.model.nomen} SA`;
     this.saveFileAs(fileName, pdf, 'application/pdf');
     this.flashMessages.success("Downloaded!");
   }).drop(),
--- app/components/round-details.js
+++ app/components/round-details.js
@@ -1,8 +1,22 @@
 import Component from '@ember/component';
 import { not } from '@ember/object/computed';
+import { task, timeout } from 'ember-concurrency';
 
 export default Component.extend({
   isDisabled: not(
     'model.permissions.write',
   ),
+  autosave: task(function* (value){
+    let round = yield this.model;
+    round.set('date', value);
+    yield timeout(1000);
+    try {
+      yield round.save();
+      this.flashMessages.success("Saved");
+    } catch(e) {
+      e.errors.forEach((error) => {
+        this.flashMessages.danger(error.detail);
+      })
+    }
+  }).restartable(),
 });
