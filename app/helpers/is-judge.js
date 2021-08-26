cc417b956ec953a77dcb0056818852631cb898dd
Alex Rubin <alex@webgraph.com>
2021-08-26 08:42:53 +0100

    Updated Access to allow CA’s to view previous conventions.

--- app/helpers/is-judge.js
+++ app/helpers/is-judge.js
@@ -1,7 +1,7 @@
 import { helper } from '@ember/component/helper';
 
 export function isJudge(params) {
-  if ((params[0].length > '1' || !params[0].includes('Manager')) && !params[0].includes('BS Admin')) {
+  if ((params[0].length > '1' || !params[0].includes('Manager')) && !params[0].includes('BS Admin') && !params[0].includes('CA')) {
     return true;
   }
   return false;
