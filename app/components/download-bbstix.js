9be1f6d82b5c96b215d8f308575f49f5b6144b7e
Alex Rubin <alex@webgraph.com>
2021-10-06 23:04:07 -0400

    Change file type to TXT for BBSTIX files.

--- app/components/download-bbstix.js
+++ app/components/download-bbstix.js
@@ -6,9 +6,9 @@ import { inject as service } from '@ember/service';
 export default Component.extend(FileSaverMixin,{
   flashMessages: service(),
   bbstix: task(function *() {
-    let docx = yield this.model.bbstix();
+    let txt = yield this.model.bbstix();
     let fileName = `${this.model.baseFilename}_BBStix`;
-    this.saveFileAs(fileName, docx, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+    this.saveFileAs(fileName, txt, 'text/plain');
     this.flashMessages.success("Downloaded!");
   }).drop(),
 });
