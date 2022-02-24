e68dfe93aa7476f3e6049024c10f72b3df769ed1
Alex Rubin <alex@webgraph.com>
2022-02-24 12:05:58 -0500

    Spots for rounds other than “Finals” can now be edited in the front end.

---
+++ app/components/round-spots.js
@@ -0,0 +1,18 @@
+import Component from '@ember/component';
+import { task, timeout } from 'ember-concurrency';
+
+export default Component.extend({
+  autosave: task(function* (value){
+    let round = yield this.model;
+    round.set('spots', value);
+    yield timeout(1000);
+    try {
+      yield round.save();
+      this.flashMessages.success("Saved");
+    } catch(e) {
+      e.errors.forEach((error) => {
+        this.flashMessages.danger(error.detail);
+      })
+    }
+  }).restartable(),
+});
