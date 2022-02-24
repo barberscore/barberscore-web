c8088704f653a872ae54647c03e76db675d1d9cf
Alex Rubin <alex@webgraph.com>
2022-02-24 12:23:35 -0500

    Disables changing of spots after the round has been completed.

--- app/components/round-spots.js
+++ app/components/round-spots.js
@@ -1,7 +1,14 @@
 import Component from '@ember/component';
 import { task, timeout } from 'ember-concurrency';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  isDisabled: computed(
+    'model',
+    function() {
+      return ['Completed', 'Finalized', 'Published'].includes(this.get('model.status'));
+    }
+  ),
   autosave: task(function* (value){
     let round = yield this.model;
     round.set('spots', value);
