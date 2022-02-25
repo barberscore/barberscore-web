b284aeb9c8603b9ab9cde1da55f245f4b878c732
Alex Rubin <alex@webgraph.com>
2022-02-24 20:28:53 -0500

    CSA file name update

--- app/components/download-csa.js
+++ app/components/download-csa.js
@@ -7,7 +7,8 @@ export default Component.extend(FileSaverMixin,{
   flashMessages: service(),
   csa: task(function *() {
     let pdf = yield this.model.csa();
-    let fileName = `${this.model.name} CSA`;
+    let groupName = this.model.name.replace(/[^a-zA-Z0-9_ ]/gi, '');
+    let fileName = `${groupName} CSA`;
     this.saveFileAs(fileName, pdf, 'application/pdf');
     this.flashMessages.success("Downloaded!");
   }).drop(),
