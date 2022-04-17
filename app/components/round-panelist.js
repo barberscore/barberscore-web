1846b7ea43a4d18904f2d490b8b5641b17f3d13f
Alex Rubin <alex@webgraph.com>
2022-04-16 22:55:51 -0400

    Allow updating of panelist numbers via scoring manager

--- app/components/round-panelist.js
+++ app/components/round-panelist.js
@@ -1,10 +1,20 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import { sort } from '@ember/object/computed';
-import { task } from 'ember-concurrency';
+import { task, timeout } from 'ember-concurrency';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  store: service(),
   router: service(),
+  isDisabled: true,
+  savePanelistError: false,
+  isScoringJudge: computed(
+    'model',
+    function() {
+      return ['Music', 'Performance', 'Singing'].includes(this.get('model.category'));
+    }
+  ),
   deletePanelist: task(function *(panelist) {
     try {
       yield panelist.destroyRecord();
@@ -23,4 +33,35 @@ export default Component.extend({
     'model.round.panelists',
     'sortedPanelistsProperties'
   ),
+  autosave: task(function* (field, value){
+    const IsNumeric = (num) => /^-{0,1}\d*\.{0,1}\d+$/.test(num);
+    yield timeout(1000);
+    try {
+      if (!IsNumeric(value)) {
+        throw { errors: [{detail: 'The number must be numeric2.'}] };
+      }
+    } catch(e) {
+      e.errors.forEach((error) => {
+        this.flashMessages.danger("Problem!");
+        this.set('savePanelistError', error.detail);
+      })
+      // if not numeric return
+      return;
+    }
+
+    this.model.set(field, value);
+    try {
+      yield this.model.save();
+      this.flashMessages.success("Saved");
+      this.set('savePanelistError', false);
+    } catch(e) {
+      this.flashMessages.danger("Problem!");
+      this.set('savePanelistError', e.errors.status);
+    }
+  }).restartable(),
+  actions: {
+    toggleNumber(){
+      this.toggleProperty('isDisabled');
+    },
+  }
 });
