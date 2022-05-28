8eb5f0f09cfa8ee44c3e06a888654efd3a04c4f9
Alex Rubin <alex@webgraph.com>
2022-05-28 09:21:08 -0400

    Added reloading of session when rounds are published.

--- app/components/round-publish.js
+++ app/components/round-publish.js
@@ -5,6 +5,7 @@ import { task } from 'ember-concurrency';
 export default Component.extend({
   currentUser: service(),
   store: service(),
+  router: service(),
   flashMessages: service(),
   publishRoundModal: false,
   publishRoundModalError: false,
@@ -17,6 +18,7 @@ export default Component.extend({
       this.set('publishRoundModal', false);
       this.set('publishRoundModalError', false);
       this.flashMessages.success("Published!");
+      this.store.findRecord('session', this.model.sessionId, { reload: true });
     } catch(e) {
       this.set('publishRoundModalError', true);
     }
