695fd44e52c2864981c90ad230be85891b0271d8
Aswin Murugesh <aswinmurugesh94@gmail.com>
2025-03-19 18:23:10 +0530

    Disable nav buttons during navigation

--- app/components/prev-next.js
+++ app/components/prev-next.js
@@ -1,10 +1,17 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { later } from '@ember/runloop';
 
 export default Component.extend({
   router: service(),
+  disableButtons: false,
   actions: {
     trans(model) {
+      const that = this;
+      this.set('disableButtons', true);
+      later(() => {
+        that.set('disableButtons', false);
+      }, 500);
       this.router.transitionTo(this.router.currentRouteName, model.get('id'));
     }
   }
