219ac31c7e47009b642384f4afe08c52446f1e73
Alex Rubin <alex@webgraph.com>
2026-04-16 18:23:49 -0400

    updated naming for OSS.

--- app/components/download-oss.js
+++ app/components/download-oss.js
@@ -5,8 +5,10 @@ import { inject as service } from '@ember/service';
 export default Component.extend({
   flashMessages: service(),
   fileDownload: service(),
-  filename: function(name) {
-    return `${name} OSS`
+  store: service(),
+  filename: function(name, sessionKind, roundKind) {
+    let suffix = `${(sessionKind || '').charAt(0)}${(roundKind || '').charAt(0)}`;
+    return `${name} ${suffix} OSS`
     .replace(/ /g,'-')
     .replace(/_/g,'-')
     .replace(/[^\w-]+/g,'')
@@ -14,7 +16,16 @@ export default Component.extend({
   },
   oss: task(function *(paperSize) {
     let model = this.model;
-    let fileName = this.filename(model.scoresheetFilename);
+    let sessionKind = model.sessionKind || '';
+    let conventionName = '';
+    if (model.conventionId) {
+      let convention = this.store.peekRecord('convention', model.conventionId);
+      if (convention) {
+        conventionName = convention.get('nomen') || '';
+      }
+    }
+    let baseName = `${conventionName} ${sessionKind} ${model.kind}`.trim();
+    let fileName = this.filename(baseName);
     yield this.fileDownload.downloadFile(this.model, 'oss', fileName, 'application/pdf', { paperSize: paperSize });
     this.flashMessages.success("Downloaded!");
   }).drop(),
